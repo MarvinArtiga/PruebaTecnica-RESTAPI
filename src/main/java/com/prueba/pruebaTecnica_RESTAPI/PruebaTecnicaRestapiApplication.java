cfef2417bb6327ebd554b717c8478407d82110c0
MarvinArtiga <marvin.artigagonzalez@gmail.com>
2026-06-15 22:36:32 -0600

    Initial Commit

---
+++ src/main/java/com/prueba/pruebaTecnica_RESTAPI/PruebaTecnicaRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.pruebaTecnica_RESTAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaRestapiApplication.class, args);
+	}
+
+}
